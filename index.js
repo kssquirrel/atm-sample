0015455e027fbf3695c128c658ca2cfbb6d8020c
Katherine Schmitt <kssquirrel@gmail.com>
2024-03-04 20:55:12 -0500

    Moved ATM to atm-sample

---
+++ index.js
@@ -0,0 +1,16 @@
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    "How much do you want to withdraw?"
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
